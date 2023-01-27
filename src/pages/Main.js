0c185f7ea06af8ea72295ef5baf5cfe4dd3240dc
20jun <ex_xe@naver.com>
2023-01-27 23:46:55 +0900

    styled-component에 미디어 쿼리 적용

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import LOGO from '../assets/images/haerin.jpg';
+
+import { IoIosMenu } from 'react-icons/io';
 
 function Main() {
-	return <h1>Hello World</h1>;
+	const [menu, setMenu] = useState(false);
+
+	return (
+		<>
+			<Common>
+				<Home href="/">강꼬양이</Home>
+				<LogoImg src={LOGO} />
+
+				<ContlorBox menu={menu}>
+					<Contlor href="/connect">계약</Contlor>
+					<Contlor href="/login">로그인</Contlor>
+					<Contlor href="/signup">회원가입</Contlor>
+				</ContlorBox>
+				<Menubar
+					href="#"
+					onClick={() => {
+						setMenu(!menu);
+					}}
+				>
+					<IoIosMenu />
+				</Menubar>
+			</Common>
+		</>
+	);
 }
+
+const Menubar = styled.a`
+	display: flex;
+	align-items: center;
+	font-size: 30px;
+	position: absolute;
+	right: 32px;
+	height: 97px;
+	@media screen and (min-width: 500px) {
+		display: none;
+	}
+`;
+
+const Common = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin: 20px;
+	@media screen and (max-width: 500px) {
+		flex-direction: column;
+	}
+`;
+
+const Home = styled.a`
+	display: flex;
+	align-items: center;
+	font-size: 30px;
+	margin: 10px;
+	text-decoration: none;
+	color: black;
+	font-weight: bold;
+	@media screen and (max-width: 500px) {
+		display: none;
+	}
+`;
+const LogoImg = styled.img`
+	width: 100px;
+`;
+
+const ContlorBox = styled.div`
+	display: flex;
+	align-items: center;
+	@media screen and (max-width: 500px) {
+		flex-direction: column;
+		align-items: flex-end;
+		display: ${({ menu }) => {
+			return menu === false ? 'none' : 'flex';
+		}};
+	}
+`;
+
+const Contlor = styled.a`
+	margin: 10px;
+	text-decoration: none;
+	color: black;
+`;
 
 export default Main;
